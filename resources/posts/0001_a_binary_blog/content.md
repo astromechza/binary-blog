65342aa0dec719410a483d5456d8927b2eefe7a2
Ben Meier <benmeier@fastmail.com>
2023-03-21 17:06:30 +0000

    small progress

---
+++ resources/posts/0001_a_binary_blog/content.md
@@ -0,0 +1,10 @@
+<meta x-title="A binary blog"/>
+<meta x-date="2023-04-01T00:00:00Z"/>
+
+Blah blah
+
+**markdown** blah
+
+---
+
+![crab](rustacean-orig-noshadow.png)
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-use clap::crate_name;
-use clap::crate_version;
-use clap::Parser;
-use convert_case::{Case, Casing};
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use lazy_static::lazy_static;
-use regex::Regex;
 use std::collections::HashMap;
-use std::convert::Infallible;
 use std::net::{IpAddr, SocketAddr};
-use maud::{html, Markup, DOCTYPE};
+use std::str::from_utf8;
+use std::sync::Arc;
 
-pub mod posts;
+use axum::{Error, response::Html, Router, routing::get};
+use axum::extract::{Path, State};
+use axum::http::{HeaderValue, StatusCode};
+use axum::response::{IntoResponse, Response};
+use chrono::FixedOffset;
+use clap::Parser;
+use maud::{DOCTYPE, html, Markup, PreEscaped};
+use rust_embed::RustEmbed;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber;
+
+#[derive(RustEmbed)]
+#[folder = "resources/"]
+struct Asset;
 
 #[derive(Parser, Debug)]
 #[command(version)]
@@ -23,115 +28,163 @@ struct Cli {
     bind_port: Option<u16>,
 }
 
-macro_rules! banner_format {
-    () => {
-        "        \
-        _   ,_,   _\n       \
-       / `'=) (='` \\\n      \
-      /.-.-.\\ /.-.-.\\\n      \
-      `      \"      `\n\
-{}: {} https://github.com/astromechza/binary-blog\n\
-args: {:?}"
-    };
+struct Post {
+    path: String,
+    title: String,
+    date: chrono::DateTime<FixedOffset>,
+    content: Markup,
 }
 
-lazy_static! {
-    static ref PAGE_RE: Regex = Regex::new("^/page/[^/]+$").unwrap();
-    static ref PAGE_ASSET_RE: Regex = Regex::new("^/page/[^/]+/[^/]+$").unwrap();
-    static ref POSTS_BY_TITLE: HashMap<String, posts::Post> = {
-        let mut m = HashMap::new();
-        let b = posts::_0001_a_binary_blog::build();
-        m.insert(b.title.to_case(Case::Snake), b);
-        m
-    };
+struct SharedState {
+    posts: Vec<Post>,
+    post_index: HashMap<String, usize>,
 }
 
-fn check_method(_req: Request<Body>, allowed: &Method) -> Option<Response<Body>> {
-    if _req.method() == allowed {
-        return None;
-    }
-    Some(Response::builder().status(405).body(Body::empty()).unwrap())
-}
+fn collect_posts() -> Vec<Post> {
+    let mut options = pulldown_cmark::Options::empty();
+    options.insert(pulldown_cmark::Options::ENABLE_STRIKETHROUGH);
 
-fn generate(title: String, inner: Markup) -> Markup {
-    html! {
-        (DOCTYPE)
-        html {
-            head {
-                meta charset="utf-8";
-                title { (title) }
+    let title_re = regex::Regex::new(r#"<meta x-title="(.+)"/?>"#).unwrap();
+    let date_re = regex::Regex::new(r#"<meta x-date="(.+)"/?>"#).unwrap();
+
+    Asset::iter()
+        .filter(|x| x.ends_with("content.md"))
+        .map(|x| {
+            let path = x
+                .split("/")
+                .take_while(|y| !(*y == "content.md"))
+                .last()
+                .unwrap();
+
+            let raw_bytes = Asset::get(&x).unwrap();
+            let raw_content = from_utf8(raw_bytes.data.as_ref()).unwrap();
+
+            let parsed_title = title_re
+                .captures(raw_content)
+                .map(|c| c.get(1).unwrap().as_str())
+                .unwrap_or("unknown");
+
+            let parsed_date = date_re
+                .captures(raw_content)
+                .map(|c| c.get(1).unwrap().as_str())
+                .map(|c| chrono::DateTime::parse_from_rfc3339(c).unwrap())
+                .unwrap_or(chrono::DateTime::default());
+
+            // TODO: find a way to parse/scan the content and populate the date / title fields
+
+            let parser = pulldown_cmark::Parser::new_ext(raw_content, options);
+            let mut html_output = String::new();
+            pulldown_cmark::html::push_html(&mut html_output, parser);
+            let tree: Markup = PreEscaped { 0: html_output };
+
+            Post {
+                path: path.to_string(),
+                title: parsed_title.to_string(),
+                date: parsed_date,
+                content: tree,
             }
-            body {
-                { (inner) }
+        })
+        .collect()
+}
+
+fn build_shared_state(posts: Vec<Post>) -> SharedState {
+    let mut post_index = HashMap::new();
+    let mut i = 0;
+    for x in &posts {
+        post_index.insert(x.path.clone(), i);
+        i = i + 1;
+    }
+
+    SharedState { posts, post_index }
+}
+
+async fn list_posts(state: State<Arc<SharedState>>) -> Html<String> {
+    Html(
+        html! {
+            (DOCTYPE)
+            html {
+                head {
+                    title { ("Index") }
+                }
+                body {
+                    h1 { "Hello, World!" }
+                    ul {
+                        @for x in state.0.posts.iter() {
+                            li {
+                                a href={ "posts/" (x.path) } {
+                                    (x.title) (x.date.to_string())
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-    }
+            .into_string(),
+    )
 }
 
-async fn handle_blog_index(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    if let Some(cm) = check_method(_req, &Method::GET) {
-        return Ok(cm);
-    }
-
-    let mut doc: Markup = generate("Index".into(), html! {
-    });
-
-    Ok(Response::builder()
-        .body(doc.into_string().into())
-        .unwrap())
+async fn view_post(Path(post_key): Path<String>, state: State<Arc<SharedState>>) -> Result<Html<String>, StatusCode> {
+    state.post_index.get(post_key.as_str())
+        .map(|i| state.posts.get(*i).unwrap())
+        .map(|p| {
+            Ok(Html(html! {
+                html {
+                    (p.content)
+                }
+            }.into_string()))
+        })
+        .unwrap_or(Err(StatusCode::NOT_FOUND))
 }
 
-async fn blog_service(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let mut resp = Response::default();
-    match _req.uri().path() {
-        "/" => handle_blog_index(_req).await,
-        x if PAGE_RE.is_match(x) => {
-            *resp.body_mut() = "here's a blog".into();
-            Ok(resp)
-        }
-        x if PAGE_ASSET_RE.is_match(x) => {
-            *resp.body_mut() = "here's a blog asset".into();
-            Ok(resp)
-        }
-        _ => Ok(Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(Body::empty())
-            .unwrap()),
-    }
+async fn view_asset(Path(key): Path<(String, String)>) -> Response {
+    let mut path: String = "posts/".to_owned();
+    path.push_str(key.0.as_str());
+    path.push('/');
+    path.push_str(key.1.as_str());
+    Asset::get(path.as_str())
+        .map(|a| {
+            let guess = mime_guess::from_path(path);
+
+            let mut resp = a.data.into_response();
+            if !guess.is_empty() {
+                let val = HeaderValue::from_str(guess.first_raw().unwrap()).unwrap();
+                resp.headers_mut().insert("content-type", val);
+            }
+            resp
+        })
+        .unwrap_or_else(|| {
+            StatusCode::NOT_FOUND.into_response()
+        })
 }
 
 #[tokio::main]
 async fn main() {
     let args = Cli::parse();
-    println!(banner_format!(), crate_name!(), crate_version!(), args);
-    //
-    // let post = posts::_0001_a_binary_blog::build();
-    //
-    // let mut options = Options::empty();
-    // options.insert(Options::ENABLE_STRIKETHROUGH);
-    // let parser = CmarkParser::new_ext(post.raw_markdown, options);
-    //
-    // let mut html_output = String::new();
-    // html::push_html(&mut html_output, parser);
-    //
-    // println!("{}", html_output);
+    tracing_subscriber::fmt::init();
 
+    let state = Arc::new(build_shared_state(collect_posts()));
+
+    let app = Router::new()
+        .route("/", get(list_posts))
+        .route("/posts/:post_key", get(view_post))
+        .route("/posts/:post_key/:asset_key", get(view_asset))
+        .with_state(state)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(
+                    tower_http::trace::DefaultMakeSpan::new().level(tracing::Level::INFO),
+                )
+                .on_response(
+                    tower_http::trace::DefaultOnResponse::new().level(tracing::Level::INFO),
+                ),
+        );
     let addr = SocketAddr::from((
         args.bind_address.unwrap_or([127, 0, 0, 1].into()),
         args.bind_port.unwrap_or(8080),
     ));
-
-    let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(blog_service))
-    });
-
-    let server = Server::bind(&addr).serve(make_svc);
-
-    println!("listening on {}...", server.local_addr());
-
-    // Run this server for... forever!
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
